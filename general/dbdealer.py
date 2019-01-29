1f833ee4ccedba60a653ed80f542400dd908a982
Guleii <424307293@qq.com>
2019-01-29 22:59:49 +0800

    解决连接数据库，增加信息，打印信息

--- general/dbdealer.py
+++ general/dbdealer.py
@@ -1,23 +1,27 @@
 import sqlite3
 
+class DBManager:
 
-def db_select(self, db_name='', tb_name=''):
+    def db_select_all(self, db_name='', tb_name=''):
 
-    # 建立数据库连接
-    conn = sqlite3.connect(db_name)
-    # 锚定位
-    cursor = conn.cursor()
-    # 执行查询语句
-    cursor.execute('''
-            SELECT * FROM 
-    ''')
-    # 打印信息
-    cursor.fetchall()
-    cursor.rowcount()
-    # 关闭cursor
-    cursor.close()
-    # 关闭连接
-    conn.close()
+        # 建立数据库连接
+        conn = sqlite3.connect(db_name)
+        # 锚定位
+        cursor = conn.cursor()
+        # 执行查询语句
+        cursor.execute('''
+                SELECT * FROM 
+        ''')
+        # 打印信息
+        cursor.fetchall()
+        # 关闭cursor
+        cursor.rowcount()
+        cursor.close()
+        # 关闭连接
+        conn.close()
+
+
+def db_create_tb(self, tb_name=''):
 
 
 if __name__ == '__main__':
